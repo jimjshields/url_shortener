6aeb617f6206cf80047114d67a7d3d08dc696ff0
jimjshields <jim.j.shields@gmail.com>
2015-01-30 14:27:59 -0500

    Added view for submitting the shorten URL form.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/shorten_url', methods=['GET', 'POST'])
+def shortenURL():
+	newUrl = request.form['url']
+	return render_template('shorten_url.html', newUrl=newUrl)
